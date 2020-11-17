756e642e03770e068286a314fd6bb38507b9f7bd
reginaldo <reginaldobrain@gmail.com>
2020-11-17 13:53:37 -0300

    firma 1711 1353

--- resources/views/usuarios_login.blade.php
+++ resources/views/usuarios_login.blade.php
@@ -19,7 +19,7 @@
 			    <label for="exampleInputPassword1">Password</label>
 			    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
 			  </div>
-			  <button type="submit" class="btn btn-default">Submit</button>
+			  <button type="submit" class="btn btn-default">Enviar</button>
 			</form>
     		
     	</dir>
