cf75fb695a790258c3faf7c7f372ccbacb142640
reginaldo <reginaldobrain@gmail.com>
2020-11-17 15:16:02 -0300

    firma 1711 1515

--- resources/views/usuarios_login.blade.php
+++ resources/views/usuarios_login.blade.php
@@ -1,3 +1,4 @@
+
 @extends('layouts.app') 
 
 @section('conteudo')
@@ -9,17 +10,18 @@
 	</div>
 
     <div class='row'>
-    	<dir class="col-md-4">
+    	<dir class="col-md-4 offset-4">
+
     		<form>
 			  <div class="form-group">
-			    <label for="exampleInputEmail1">Email address</label>
-			    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			    <label for="id_usuarios">Email</label>
+			    <input type="email" class="form-control" id="id_usuarios" name="text_usuario" placeholder="Usuario:">
 			  </div>
 			  <div class="form-group">
-			    <label for="exampleInputPassword1">Password</label>
-			    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			    <label for="id_senha">Senha</label>
+			    <input type="password" class="form-control" id="id_senha" name="text_senha" placeholder="Password:">
 			  </div>
-			  <button type="submit" class="btn btn-default">Enviar</button>
+			  <button type="submit" class="btn btn-primary">login</button>
 			</form>
     		
     	</dir>
