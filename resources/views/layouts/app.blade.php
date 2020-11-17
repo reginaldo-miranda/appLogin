a953220b57332daae639bef68379072465680d49
reginaldo <reginaldobrain@gmail.com>
2020-11-17 11:35:55 -0300

    firma 1711 1135

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,17 +3,22 @@
   <head>
     <!-- Meta tags Obrigatórias -->
     <meta charset="utf-8">
+    <meta http-arquivo="X-UA-compatible" content="IE-edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
     <title>sistema</title>
+    <!-- Bootstrap CSS -->
+   <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+ 
+    <link href="{{asset('css/app.css')}}" rel="stylesheet"> 
+    <link href="{{asset('css/meu.css')}}" rel="stylesheet">  
+   
   </head>
   <body>
    
-   <div class='container'>
-     @yield('conteudo')
+   <div class="container">
+
+      @yield('conteudo') 
+
    </div>
 
     <!-- JavaScript (Opcional) -->
--- resources/views/usuarios_login.blade.php
+++ resources/views/usuarios_login.blade.php
@@ -1,7 +1,30 @@
-@extends('layouts.app')
+@extends('layouts.app') 
 
 @section('conteudo')
 
-	<h1>Teste</h1>
+	<div id="imagem">
+
+		<img src="{{asset('imagens/cadeado.png')}}">
+		
+	</div>
+
+    <div class='row'>
+    	<dir class="col-md-4">
+    		<form>
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Email address</label>
+			    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Password</label>
+			    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+			  </div>
+			  <button type="submit" class="btn btn-default">Submit</button>
+			</form>
+    		
+    	</dir>
+    	
+    </div>
+
 
 @endsection
